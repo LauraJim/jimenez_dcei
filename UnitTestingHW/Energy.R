3050e76ec3ab8070e190a186c39edcdd5e2c6807
Laura Jimenez <lau.violeta.estad@gmail.com>
2018-04-09 16:35:04 -0500

    filling in the specific code of function Energy

--- UnitTestingHW/Energy.R
+++ UnitTestingHW/Energy.R
@@ -8,21 +8,23 @@
 
 # ARGUMENTS: see NOTES
 # th --- vector that contains the current values of parameters mu and A
+# Variables Winv, alpha and dd must be defined before using this function
 
 # OUTPUTS:
 # The value of the objective function at th
 
 # NOTES:
-# This function is called right after Supp: mu, A and detA are defined,
-# so th is ignored. However, this is the way we need to specify the function
-# in order to be able to use the t-walk algorithm.
+# This function is called right after Supp so that mu, A, detA and suma.Et
+# are defined, so th is ignored. This is the right way we need to specify
+# this function in order to be able to use the t-walk algorithm.
 
 Energy <- function(th) 
 { 
-  # define terms that come from the likelihood function
-  
-  # define terms the come form the a priori densities
-  
-  # combine the two terms and return this value
-  
+  ax1 <- (mu - mu0)
+  ax2 <- apply( env.sp, 1, function(xi) { ax<-as.matrix(xi - mu); t(ax) %*% A %*% ax })
+  # first two terms are generic in the posterior due to the normal model
+  S <- 0.5*sum(ax2) + n*log(suma.Et)
+  # these terms correspond to the priors:
+  S <- S + 0.5*( t(ax1) %*% A0 %*% ax1 + sum(diag(A %*% Winv)) - (alpha-dd-1)*log(detA) )
+  S
 }
