4b1319c20b748fa21e4497637f216ce91a939581
Laura Jimenez <lau.violeta.estad@gmail.com>
2018-04-09 16:36:08 -0500

    small changes in comments of function Initth

--- UnitTestingHW/Initth.R
+++ UnitTestingHW/Initth.R
@@ -6,7 +6,7 @@
 # mu and a Wishart distribution for A.
 
 # ARGUMENTS: see NOTES
-# NONE
+# NONE but it uses the variables CholW, mu0 and CholSigma
 
 # OUTPUTS: see NOTES
 # A numeric vector of length = lenght(mu) + (ncol(A)*(ncol(A)+1)/2)
@@ -16,14 +16,17 @@
 # it can not include more arguments or have a different kind of output.
 # 2) The method used to produce the random samples from the a priori distributions
 # can be different, I will start with a method that works for 2 dimensions.
+
 # CODE:
 Initth <- function()
 {
   # Use mu0 and Sigma0 to get a random value from the a priori multivariate normal distribution
-  
-  # Use the random value from the previous step to get a random matrix from the a priori Wishart distribution
-  
+  mu <- mu0 + CholSigma0 %*% rnorm(2)
+  # Use CholW to get a random matrix from the a priori Wishart distribution
+  X1 <- CholW %*% rnorm(2)
+  X2 <- CholW %*% rnorm(2)
+  A <- X1 %*% t(X1) + X2 %*% t(X2)  ### This works for alpha = 2!!!
   # Combine all the random values in a single vector called 'random'
-  
+  c( mu[1], mu[2], A[1,1], A[2,2], A[1,2])
   # Return 'random'
 }
